064df080fa890ad7da7c88b0947b143b95fa79ed
Luke Keller <lukeekeller@gmail.com>
2014-03-29 17:09:31 -0400

    article new functionality and form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,23 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
   end
 
-  def create
+  def new
+      @article = Article.new
   end
 
-  def new
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render "new"
+    end
   end
+
+  private
+
+    def article_params
+        params.require(:article).permit(:title, :content, :category_ids =>[])
+    end
 
 end
